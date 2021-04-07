f71352e658ddfa0281baff93b0d3742531a9bf77
Yi Li <leeoh12784@gmail.com>
2021-04-06 23:21:40 -0400

    spad_wr child instruction bug fixed

--- src/spad_instr.cc
+++ src/spad_instr.cc
@@ -159,7 +159,7 @@ void DefineSPADInstrChild(Ila& m) {
 
   { // child instruction for reading data from external memory to SPAD1
     auto instr = child.NewInstr("spad_1_child_wr");
-    instr.SetDecode(valid_flag == 1 & cntr < rd_wr_length & target == 0);
+    instr.SetDecode(valid_flag == 1 & cntr < rd_wr_length & target == 1);
 
     // this part model the AXI master interface addr port
     // assume the cntr*16 value wouldn't overflow
